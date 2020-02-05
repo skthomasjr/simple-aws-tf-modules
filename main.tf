797fdb51603a3c3194a4b74303c904e595c382d0
Isa Aguilar <isa@aguilarstory.com>
2020-02-04 16:18:49 -0800

    main module

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region  = "${var.region}"
+  profile = "${var.profile}"
+}
+
+# ----- Bucket -----
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.bucket}" # Current var is name
+  acl    = "${var.acl}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = "${var.versioning}"
+  }
+
+  cors_rule      = "${var.cors_rule}"
+  tags           = "${var.tags}"
+  lifecycle_rule = "${var.lifecycle_rules}"
+}
+
+
